0588f9e489babef3fc8677389e3fe7103eddb2c5
Astronaut <66795634+Anvozz@users.noreply.github.com>
2022-03-30 01:30:26 +0700

    feat(LayoutExcluder) : initial commit

---
+++ utils/LayoutExcluder.js
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router"
+
+/**
+ * Function นี้เป็น Function ที่ใช้งานร่วมกับ Loyout 
+ * เป็น Function ที่เช็ค Router.pathname ว่าไม่ต้องการ จะใช้ Layout หน้าไหนบ้าง
+ * วิธีใช้ ให้นำ ชื่อ path ที่ไม้ต้องการจะใช้ Layout ไปใส่ใน Array ชื่อที่ชื่อว่า Exclude = []
+ */
+
+const Exclude = [
+  '/login',
+  '/'
+]
+
+const LayoutExcluder = () => {
+  const router = useRouter()
+
+  const isFound = Exclude.find( e => e === router.pathname )
+
+  return isFound ? false : true
+}
+
+export default LayoutExcluder
