a2b1d89c7b1e170db1cf834b84a9a3a3cb43f99f
Astronaut <66795634+Anvozz@users.noreply.github.com>
2022-03-30 18:04:21 +0700

    fix (header) : :+1:

--- components/Layout/CustomHeader.jsx
+++ components/Layout/CustomHeader.jsx
@@ -1,6 +1,5 @@
 import { Layout , Divider , Dropdown , Menu , Space , Tooltip} from 'antd'
 import { MenuFoldOutlined , MenuUnfoldOutlined , DownOutlined , UserOutlined , SettingOutlined} from '@ant-design/icons';
-import Image from 'next/image'
 import { useRouter } from 'next/router';
 import _localStorage from '../../utils/BrowserLocalstorage';
 
@@ -62,7 +61,7 @@ const CustomHeader = ({CollapsedToggle , collapsed}) => {
             alignItems: "center",
           }}
         >
-          <Image
+          <img
             src="/assets/images/PTT Public Company Limited.svg"
             width={120}
             height={60}
