1d326dc709ae039cd3c96d84a4936b276157a379
Astronaut <66795634+Anvozz@users.noreply.github.com>
2022-04-07 00:40:10 +0700

    card

--- pages/Frontoffice/analysis_graph.jsx
+++ pages/Frontoffice/analysis_graph.jsx
@@ -1,9 +1,22 @@
-
+import { Row , Col , Card , Skeleton} from 'antd'
 
 const AnalysisGraph = () =>{
     return (
         <>
-            This Analysis graph Page
+            <Row gutter={8}>
+                <Col xl={12}>
+                    <Card >
+                        <p>Card content</p>
+                        <p>Card content</p>
+                        <p>Card content</p>
+                    </Card>
+                </Col>
+                <Col xl={12}>
+                    <Card>
+                        <Skeleton/>
+                    </Card>
+                </Col>
+            </Row>
         </>
     )
 }
