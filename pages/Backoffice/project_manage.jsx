997583fb7ae33e86e86a78fbac544111539de8c3
Thanongsak27 <zamidoku@gmail.com>
2022-05-26 10:44:53 +0700

    update

--- pages/Backoffice/project_manage.jsx
+++ pages/Backoffice/project_manage.jsx
@@ -1,34 +1,24 @@
 import React, { useState , useEffect } from 'react'
 import { Card , Space , Table , Tag, Dropdown, Button, Menu } from 'antd';
-import { dataz } from '../../config/data_company';
 
 const ProjectManage = (props) => {
     
 
     const columns = [
         {
-          title: 'รหัสบริษัท',
-          dataIndex: 'reg_company_id',
-          key: 'reg_company_id',
+          title: 'รหัสโครงการ',
+          dataIndex: 'project_company_id',
+          key: 'project_company_id',
         },
         {
-          title: 'ชื่อบริษัท',
-          dataIndex: 'company_name_th',
-          key: 'company_name_th',
+          title: 'ชื่อโครงการ',
+          dataIndex: 'project_name_th',
+          key: 'project_name_th',
         },
         {
-          title: 'ที่อยู่',
-          dataIndex: 'village_building_th',
-          key: 'village_building_th',
-        },
-        {
-          title: "สถานะ",
-          dataIndex: "status",
-          key: "status",
-          sorter: (a, b) => a.type - b.type,
-          render: (record) => {
-            return <p style={{ backgroundColor: record ? '#6CFF60' : '#FF6060', textAlign: 'center', borderRadius: '10px', color: 'white' }}>{record ? "ใช้งาน" : "ไม่ได้ใช้งาน"}</p>;
-          },
+          title: 'ผู้รับผิดชอบ',
+          dataIndex: 'respon_person',
+          key: 'respon_person',
         },
     ];
 
@@ -36,7 +26,7 @@ const ProjectManage = (props) => {
         <>
         <Card style={{ marginTop : '1rem' }} bordered={true}>
             <h1>จัดการข้อมูลโครงการ</h1>
-            <Table columns={columns} dataSource={dataz} expandable size={'middle'} />
+            <Table columns={columns} expandable size={'middle'} />
         </Card>
         </>
     )
