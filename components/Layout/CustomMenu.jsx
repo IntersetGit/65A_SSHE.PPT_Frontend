ee78bfe96aabdc2ad819e57ab9d0f23408945fb1
Astronaut <66795634+Anvozz@users.noreply.github.com>
2022-06-02 18:06:48 +0700

    upadte

--- components/Layout/CustomMenu.jsx
+++ components/Layout/CustomMenu.jsx
@@ -2,6 +2,8 @@ import { Menu } from "antd";
 import Icon from '@ant-design/icons';
 import Link from 'next/link';
 import PTTBACKGROUND from '../../public/assets/bg-section-3-black.png'
+import { useRouter } from "next/router"
+import { useEffect, useState } from "react";
 
 /*
 * TODO: เพิ่ม path resolver สำหรับ select Menu
@@ -10,34 +12,42 @@ import PTTBACKGROUND from '../../public/assets/bg-section-3-black.png'
 const { SubMenu } = Menu;
 
 const CustomMenu = ({Menuitems}) => {
+    const [selectedKeys,setSelectedkeys] = useState('')
+    const router = useRouter()
 
+    useEffect(() =>{
+        setSelectedkeys(router.pathname.split('/')[2])
+        console.log(router.pathname.split('/')[2])
+    },[router.pathname])
   return (
     <>
       <Menu
-            selectable={false}
-            multiple
+            // selectable={false}
+            // multiple
             theme="dark"
             mode="inline"
-            // defaultSelectedKeys={["1"]}
-            style={{ height: "100%", borderRight: 0, fontSize: "0.9rem" , fontWeight : "200" , overflowX : 'hidden' , backgroundImage : `url(${PTTBACKGROUND})` , backgroundRepeat : 'no-repeat' , backgroundSize : 'cover' , backgroundPosition : 'center' }}
+            defaultOpenKeys={['sshe_analysis_submenu']}
+            defaultSelectedKeys={selectedKeys}
+            selectedKeys={selectedKeys}
+            style={{ height: "100%", borderRight: 0, fontSize: "1rem" , fontWeight : "normal" , overflowX : 'hidden' , backgroundImage : `url(${PTTBACKGROUND})` , backgroundRepeat : 'no-repeat' , backgroundSize : 'cover' , backgroundPosition : 'center' }}
       >
         {
           Menuitems.menu.map((value,key) =>{
             return(
                 value.title ?
-                    <Menu.ItemGroup key={`${Menuitems.pathname}-${key}`} title={<span style={{color : 'white' , fontWeight : '300'  , padding : '3px 6px 6px 6px' , borderRadius : '10px'}}>{value.title}</span>}>
+                    <Menu.ItemGroup key={`${Menuitems.pathname}-${key}`} title={<span style={{color : 'white' , fontSize : '1rem', fontWeight : 'normal'  , padding : '3px 6px 6px 6px' , borderRadius : '10px'}}>{value.title}</span>}>
                         {value.list.map((v , k) =>{
                             return(
                                 v.type === 'menu' ?
-                                    <Menu.Item key={`${v.menu_name}${k+1}`} icon={<Icon component={v.icon} />}>
+                                    <Menu.Item key={`${v.menu_name}`} icon={<Icon component={v.icon} />}>
                                         <Link href={`/${Menuitems.pathname}/${v.menu_name}`}>{v.title}</Link>
                                     </Menu.Item>
                                     :
-                                    <SubMenu key={v.menu_name} icon={<Icon component={v.icon} />} title={v.title}>
+                                    <SubMenu key={`${v.menu_name}`} icon={<Icon component={v.icon} />} title={v.title}>
                                         {v.menu_list.map((_v,_k) =>{
                                             return (
                                                 _v.type === 'menu' &&
-                                                <Menu.Item icon={<Icon component={_v.icon} />} key={`${v.menu_name}_${_k}`}><Link href={`/${Menuitems.pathname}/${_v.menu_name}`}>{_v.title}</Link></Menu.Item>
+                                                <Menu.Item icon={<Icon component={_v.icon} />} key={`${_v.menu_name}`}><Link href={`/${Menuitems.pathname}/${_v.menu_name}`}>{_v.title}</Link></Menu.Item>
                                             )
                                         })
                                         }
@@ -51,15 +61,15 @@ const CustomMenu = ({Menuitems}) => {
                         {value.list.map((v , k) =>{
                             return(
                                 v.type === 'menu' ?
-                                    <Menu.Item key={`${v.menu_name}${k+1}`} icon={<Icon component={PictureIcon} />}>
+                                    <Menu.Item key={`${v.menu_name}`} icon={<Icon component={PictureIcon} />}>
                                         <Link href={`/${Menuitems.pathname}/${v.menu_name}`}>{v.title}</Link>
                                     </Menu.Item>
                                     :
-                                    <SubMenu key={v.menu_name} icon={<Icon component={v.icon} />} title={v.title}>
+                                    <SubMenu key={`${v.menu_name}`} icon={<Icon component={v.icon} />} title={v.title}>
                                         {v.menu_list.map((_v,_k) =>{
                                             return (
                                                 _v.type === 'menu' &&
-                                                <Menu.Item icon={<Icon component={_v.icon} />} key={`${v.menu_name}_${_k}`}><Link href={`/${Menuitems.pathname}/${_v.menu_name}`}>{_v.title}</Link></Menu.Item>
+                                                <Menu.Item icon={<Icon component={_v.icon} />} key={`${_v.menu_name}`}><Link href={`/${Menuitems.pathname}/${_v.menu_name}`}>{_v.title}</Link></Menu.Item>
                                             )
                                         })
                                         }
