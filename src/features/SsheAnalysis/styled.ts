304b88787b846f5870e438c24ebfadce5fdb36ab
sriharach <folksriharach@gmail.com>
2022-07-18 23:53:11 +0700

    analysis issue

---
+++ src/features/SsheAnalysis/styled.ts
@@ -0,0 +1,11 @@
+const headerTitle: React.CSSProperties = {
+  fontWeight: 600,
+  fontSize: 16,
+};
+
+const cardTool: React.CSSProperties = { width: '100%', height: '90%' };
+
+export default {
+  headerTitle,
+  cardTool,
+};
